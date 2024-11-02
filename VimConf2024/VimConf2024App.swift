02e30931519b001d656239cf3be7af6a5d50e03c
uhooi <the_uhooi@yahoo.co.jp>
2024-11-03 00:33:41 +0900

    Create project

---
+++ VimConf2024/VimConf2024App.swift
@@ -0,0 +1,17 @@
+//
+//  VimConf2024App.swift
+//  VimConf2024
+//
+//  Created by uhooi on 2024/11/03.
+//
+
+import SwiftUI
+
+@main
+struct VimConf2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VimConf2024Tests/VimConf2024Tests.swift
@@ -0,0 +1,17 @@
+//
+//  VimConf2024Tests.swift
+//  VimConf2024Tests
+//
+//  Created by uhooi on 2024/11/03.
+//
+
+import Testing
+@testable import VimConf2024
+
+struct VimConf2024Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
