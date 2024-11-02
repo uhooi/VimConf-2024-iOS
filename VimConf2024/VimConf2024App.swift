d7ac3dbe25af81e18dab86640f5766eaa868793f
uhooi <the_uhooi@yahoo.co.jp>
2024-11-03 01:04:52 +0900

    Add package

--- VimConf2024/VimConf2024App.swift
+++ VimConf2024/VimConf2024App.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import VimConf2024App
 
 @main
 struct VimConf2024App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootScreen()
         }
     }
 }
