2ee817c7aa7b383d104f29eb5effc3a4dc66e0cf
uhooi <the_uhooi@yahoo.co.jp>
2025-02-14 10:28:47 +0900

    Add lightning talk speakers

--- VimConf2024Package/Sources/Data/Session/Models/LightningTalkSpeaker.swift
+++ VimConf2024Package/Sources/Data/Session/Models/LightningTalkSpeaker.swift
@@ -1,3 +1,14 @@
 package struct LightningTalkSpeaker: Sendable {
     package let name: String
 }
+
+// MARK: - Internals
+
+extension LightningTalkSpeaker {
+    static let mimikun: Self = .init(name: "mimikun")
+    static let omochice: Self = .init(name: "Omochice")
+    static let kuuote: Self = .init(name: "kuuote")
+    static let uhooi: Self = .init(name: "uhooi")
+    static let kazumaInagaki: Self = .init(name: "Kazuma Inagaki")
+    static let satoruKitaguchi: Self = .init(name: "Satoru Kitaguchi")
+}
